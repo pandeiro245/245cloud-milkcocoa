ec923342b937b144236202dd80221cab74c6003a
Hironobu Nishikokura <nishiko@mindia.jp>
2014-11-18 17:47:03 +0900

    init

---
+++ public/main.js
@@ -0,0 +1,30 @@
+var milkcocoa = new MilkCocoa("https://io-ui2n0gy4p.mlkcca.com");
+
+var chatDataStore = milkcocoa.dataStore("chat");
+var textArea, board;
+window.onload = function(){
+  textArea = document.getElementById("msg");
+  board = document.getElementById("board");
+}
+
+function clickEvent(){
+  var text = textArea.value;
+  sendText(text);
+}
+
+function sendText(text){
+  chatDataStore.push({message : text},function(data){
+    console.log("送信完了!");
+    textArea.value = "";
+  });
+}
+
+chatDataStore.on("push",function(data){
+  addText(data.value.message);
+});
+
+function addText(text){
+  var msgDom = document.createElement("li");
+  msgDom.innerHTML = text;
+  board.insertBefore(msgDom, board.firstChild);
+}
